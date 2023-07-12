67e31cc47e6a6fffe0d920e01b303509151b77e1
DaniAndhika <daniandhika9@gmail.com>
2023-07-12 23:10:07 +0700

    dani-commerce

---
+++ Assets/source.js
@@ -0,0 +1,13 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function buy(product_name, product_price) {
+    const message = `hallo saya mau beli ${product_name} - ${product_price}`
+    const whatsapp =`https://wa.me/628971483383?text=${message}`
+    open(whatsapp, "_blank")
+}
+
+function chat() {
+    const message =`halo min saya mau tanya nih `
+    const whatsapp = `https://wa.me/628971483383?text=${message}`
+    open(whatsapp, "_blank")
+}
