b60d68429acbcd0777225169553ddfe9f3aac9bf
DaniAndhika <daniandhika9@gmail.com>
2023-07-23 09:31:50 +0700

    commit 2

--- Assets/source.js
+++ Assets/source.js
@@ -2,12 +2,12 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 function buy(product_name, product_price) {
     const message = `hallo saya mau beli ${product_name} - ${product_price}`
-    const whatsapp =`https://wa.me/628971483383?text=${message}`
+    const whatsapp =`https://wa.me/6287852692092?text=${message}`
     open(whatsapp, "_blank")
 }
 
 function chat() {
     const message =`halo min saya mau tanya nih `
-    const whatsapp = `https://wa.me/628971483383?text=${message}`
+    const whatsapp = `https://wa.me/6287852692092?text=${message}`
     open(whatsapp, "_blank")
 }
